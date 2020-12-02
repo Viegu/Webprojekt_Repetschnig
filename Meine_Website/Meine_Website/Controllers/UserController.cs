59341121a41173ad451202168a17338b68f5bfda
SpiritKing <repetschnigmanuel@gmail.com>
2020-12-02 10:44:22 +0100

    register

--- Meine_Website/Meine_Website/Controllers/UserController.cs
+++ Meine_Website/Meine_Website/Controllers/UserController.cs
@@ -13,15 +13,35 @@ namespace Meine_Website.Controllers
             return View();
         }
 
-        public IActionResult MyAccount()
+        
+            public IActionResult MyAccount()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Free()
         {
-            return View();
+            return View(new Models.Account());
         }
+        [HttpPost]
+        public IActionResult Free(Models.Account newAccount)
+        {
+            // Parameter überprüfen
+            if (newAccount == null)
+            {
+                return RedirectToAction("Free");
+            }
+            ValidateAccountData(newAccount);
+
+            if (ModelState.IsValid)
+            {
+              
+                return RedirectToAction("index", "home");
+            }
+            return View(newAccount);
+        }
+        
 
         public IActionResult Standard()
         {
@@ -32,5 +52,49 @@ namespace Meine_Website.Controllers
         {
             return View();
         }
+
+        private void ValidateAccountData(Models.Account a)
+        {
+            if(a == null)
+            {
+                return;
+            }
+
+            a.Username = a.Username ?? "";
+
+            if(a.Username.Length < 4)
+            {
+                ModelState.AddModelError(nameof(Models.Account.Username),"Der Username muss mind. 5 Zeichen lang sein!");
+            }
+
+
+            if (a.Passwort != a.Passwort1)
+            {
+                ModelState.AddModelError(nameof(Models.Account.Passwort), "Die Passwörter stimmen nicht überein");
+            }
+
+            if (!a.Email.Contains("@"))
+            {
+                ModelState.AddModelError(nameof(Models.Account.Email), "Die email Stimmt nicht!!");
+            }
+
+
+            if (a.Vorname == null)
+            {
+                ModelState.AddModelError(nameof(Models.Account.Vorname), "Pflichtfeld!");
+            }
+
+            if (a.Nachname == null)
+            {
+                ModelState.AddModelError(nameof(Models.Account.Nachname), "Pflichtfeld!");
+            }
+
+            if (a.Geschlecht == Models.Geschlecht.nichtAngegeben)
+            {
+                ModelState.AddModelError(nameof(Models.Account.Nachname), "Bitte GEschlecht angeben");
+            }
+
+        }
+
     }
 }
