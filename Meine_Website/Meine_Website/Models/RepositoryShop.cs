83dc6650f21ece427d479b1e42e584ea780eb2c4
Manuel <mrepetschnig@tsn.at>
2021-06-09 08:43:48 +0200

    Update RepositoryShop.cs

--- Meine_Website/Meine_Website/Models/RepositoryShop.cs
+++ Meine_Website/Meine_Website/Models/RepositoryShop.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -35,25 +36,138 @@ namespace Meine_Website.Models
 
         public List<Article> getAllArticlesFromGenre(Genre genre)
         {
-            throw new NotImplementedException();
+            if (this._conn.State == ConnectionState.Open)
+            {
+                List<Article> articles = new List<Article>();
+
+                // leeres Command erzeugen
+                DbCommand cmdSelect = this._conn.CreateCommand();
+                cmdSelect.CommandText = "select * from articles where genre = " + genre;
+
+                using (DbDataReader reader = cmdSelect.ExecuteReader())
+                {
+
+                    // Read() liest den nächsten Record ein
+                    while (reader.Read())
+                    {
+                        articles.Add(new Article
+                        {
+                            ArticleID = Convert.ToInt32(reader["article_id"]),
+                            ArticleName = Convert.ToString(reader["name"]),
+                            Price = Convert.ToDecimal(reader["price"]),
+                            Description = Convert.ToString(reader["description"]),
+                            Genre = (Genre)Convert.ToInt32(reader["genre"]),
+                            username = Convert.ToString(reader["username"]),
+                        });
+
+                    }
+                  
+                }
+                if (articles.Count == 0)
+                {
+                    return null;
+                }
+                return articles;
+
+            }
+
+
+            throw new Exception("Datenbankfehler: Verbindung konnte nicht aufgebaut werden ");
         }
 
-        public Article GetarticleByID(int articleID)
+        public Article GetarticleByID(int articleId)
         {
-            throw new NotImplementedException();
+            if (this._conn.State == ConnectionState.Open)
+            {
+                Article article = null;
+
+                // leeres Command erzeugen
+                DbCommand cmdSelect = this._conn.CreateCommand();
+                cmdSelect.CommandText = "select * from articles where article_id = " + articleId;
+
+                using (DbDataReader reader = cmdSelect.ExecuteReader())
+                {
+
+                    // Read() liest den nächsten Record ein
+                    if (reader.Read())
+                    {
+                        article = new Article();
+
+                        article.ArticleID = Convert.ToInt32(reader["article_id"]);
+                        article.ArticleName = Convert.ToString(reader["name"]);
+                        article.Price = Convert.ToDecimal(reader["price"]);
+                        article.Description = Convert.ToString(reader["description"]);
+                        article.Genre = (Genre)Convert.ToInt32(reader["genre"]);
+                        article.username = Convert.ToString(reader["username"]);
+                    }
+                }   
+
+                return article;
+            }
+
+            
+            throw new Exception("Datenbankfehler: Verbindung konnte nicht aufgebaut werden ");
         }
+
 
         public List<Article> GetArticles()
         {
-            throw new NotImplementedException();
+
+            if (this._conn.State == ConnectionState.Open)
+            {
+                List<Article> articles = new List<Article>();
+
+                // leeres Command erzeugen
+                DbCommand cmdSelect = this._conn.CreateCommand();
+                cmdSelect.CommandText = "select * from articles order by article_id";
+
+                using (DbDataReader reader = cmdSelect.ExecuteReader())
+                {
+
+                    // Read() liest den nächsten Record ein
+                    while (reader.Read())
+                    {
+
+                        articles.Add(new Article
+                        {
+                            // article_id innerhalb der []-Klammern entspricht dem
+                            //      Feldname article_id in der DB-Tabelle
+                            ArticleID = Convert.ToInt32(reader["article_id"]),
+                            ArticleName = Convert.ToString(reader["name"]),
+                            Price = Convert.ToDecimal(reader["price"]),
+                            Description = Convert.ToString(reader["description"]),
+                            Genre = (Genre)Convert.ToInt32(reader["genre"]),
+                            username = Convert.ToString(reader["username"]),
+                        });
+
+                    }
+
+                }   // an dieser Stelle wird der reader wieder automatisch geschlossen
+
+             
+                
+                if (articles.Count == 0) {
+                    return null;
+                }
+                return articles;
+                
+            }
+
+            //return null;
+            throw new Exception("Datenbank: Verbindung ist nicht geöffnet!");
         }
 
         public bool Insert(Article art)
         {
             throw new NotImplementedException();
         }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
         //delete-> Benutzer kann eigene Uploads löschen
         //Suchen nach Artikeln mittels Ajax (Nach Genres)
-       
+
     }
 }
